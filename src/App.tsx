d208fc10bcd03b2dc9bb7dfb08abbae373bc550c
Marcos J. Botene <marcos_j_martins@estudante.sc.senai.br>
2020-11-05 20:20:34 -0200

    Added User Profile Infomations.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Container from "./pages/components/Container";
+
+import { AvatarImage } from "./pages/components/UserProfile/AvatarUser/avatarUser";
+import { UserInfo } from "./pages/components/UserProfile/UserInfo/userInfo";
 
 export default function App() {
+  const ImageURL =
+    "https://avatars0.githubusercontent.com/u/50931267?s=400&u=dc8b56b8e01b23d0a8b0f9f1ff46089f1891c047&v=4";
   return (
-    <div>
-      <h1>Testa Ai</h1>
-    </div>
+    <Container>
+      <AvatarImage
+        avatar={{ id: 1, image: ImageURL, description: "Garoto Gay" }}
+      />
+      <UserInfo
+        user={{
+          id: 1,
+          name: "Daniel Sansão Araldi",
+          nickName: "DanielAraldi",
+          bio: "Buscando sempre novas oportunidades."
+        }}
+      />
+    </Container>
   );
 }
